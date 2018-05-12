e6a1ed881d4e6691f0e43e9cc5244714b265b6c1
Tamás Németh <tomuwhu@gmail.com>
2018-05-12 17:56:29 +0200

    .

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const path = require('path')
+const bpo = { extended: true, limit:300000 }
+app.use( express.static(path.join(__dirname, 'public')) )
+app.use( bodyParser.urlencoded(bpo) )
+
+app.get( '/', (req,res) => {
+
+  res.sendFile('index.html')
+
+} )
+
+app.listen(3000)
