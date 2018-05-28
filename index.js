0a8547c38d6b8c35ab81f79e6e93ee84a725db68
Tamás Németh <tomuwhu@gmail.com>
2018-05-28 13:01:05 +0200

    .

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const session = require('express-session')
+const fs = require('fs')
 const path = require('path')
-const sfop = {
-    root: __dirname + '/private/',
-    dotfiles: 'deny',
-    headers: {
-        'x-timestamp': Date.now(),
-        'x-sent': true
-    }
-  }
+function render(req,res,target) {
+  file = `
+  <!DOCTYPE html>
+  <html>
+  <meta charset="UTF-8">
+  <link type="text/css" rel="stylesheet" href="bv/bootstrap.min.css" />
+  <link type="text/css" rel="stylesheet" href="bv/bootstrap-vue.css" />
+  <script src="bv/axios.min.js"></script><script src="bv/vue.js"></script>
+  <script src="bv/polyfill.min.js"></script><script src="bv/bootstrap-vue.js"></script>
+  <body>`
+  +fs.readFileSync(__dirname+'/private/'+target+'.vue')+
+  `</body>
+  </html>`
+  res.send(file)
+}
 express()
   .use( express.static(path.join(__dirname, 'public')) )
   .use( bodyParser.json() )
@@ -18,8 +26,6 @@ express()
         secret: 'ABC123', cookie: { maxAge: 600000 }
   }))
 
-  .get( '/', (req,res) => {
-    res.sendFile('root.html',sfop)
-  } )
+  .get( '/', (req,res) => render(req,res,'root') )
 
   .listen(3000)
