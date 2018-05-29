89fe59babe5a69cbb5cc3b41bc34dc23bf239b46
Tamás Németh <tomuwhu@gmail.com>
2018-05-29 21:37:49 +0200

    &lt; &gt;

--- src/api/server.js
+++ src/api/server.js
@@ -3,17 +3,34 @@ var bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.json())
 app.use( (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  res.header(
+    'Access-Control-Allow-Origin',
+    '*'
+  )
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
   next()
 })
 var counter = 0
-app.post('/', (req, res) => {
-  res.send({count: ++counter})
-})
+app.post(
+  '/',
+  (req, res) => {
+    res.send({count: ++counter})
+  }
+)
 
-app.get('/', (req, res) => {
-  res.send( { a: 'GET Access' } )
-})
+app.get(
+  '/',
+  (req, res) => {
+    res.send( { a: 'GET Access' } )
+  }
+)
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(
+  3000,
+  () => console.log(
+        'Back-end server listening on port 3000!'
+  )
+)
