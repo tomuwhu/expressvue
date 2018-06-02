2d1f870c9c8156a3d1980c15b036eac13ca1baf0
Tamás Németh <tomuwhu@gmail.com>
2018-06-02 21:01:02 +0200

    MySQL

--- server.js
+++ server.js
@@ -1,33 +1,59 @@
 const express = require('express')
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 const app = express()
-app.use(express.static('dist'))
-app.use(bodyParser.json())
+const sql = require('mysql').createConnection({
+      host     : 'localhost',
+      user     : 'root',
+      password : 'Cham1ss0',
+      database : 'test'
+})
+
+app.use( express.static('dist') )
+app.use( bodyParser.json() )
+
+sql.connect()
 
 //use this only in development mode!
 app.use( (req, res, next) => {
-  res.header(
-    'Access-Control-Allow-Origin',
-    '*'
-  )
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  next()
+    res.header(
+        'Access-Control-Allow-Origin',
+        '*'
+    )
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
 } )
 //do nor use it in production mode!
 
 var counter = 0
 app.post(
-  '/',
+    '/',
+    (req, res) => {
+        res.send({count: ++counter})
+    }
+)
+
+app.get(
+  '/mysqltest',
   (req, res) => {
-    res.send({count: ++counter})
+      sql.query(
+          'SELECT * FROM t1',
+          (err, rows) => {
+              if (err) {
+                res.send(err)
+              } else {
+                res.send(rows)
+              }
+          }
+      )
   }
 )
+
 app.listen(
-  3000,
-  () => console.log(
-        'Back-end server listening on port 3000!'
-  )
+    3000,
+    () => console.log(
+          'Back-end server listening on port 3000!'
+    )
 )
